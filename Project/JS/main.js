141b774b4b0662cddb1b38a1a200faf8e3060104
Farhana <farha.islam.1310gmail.com>
2023-07-31 10:47:06 -0600

    added files

--- Project/JS/main.js
+++ Project/JS/main.js
@@ -1,10 +1,38 @@
 
 
-let navbar=document.querySelector('.HeaderContainer .navcontainer')
-document.querySelector('#menu-btn').onclick=()=>{
-navbar.classList.add('active')
-}
 
-document.querySelector('#close-navbar').onclick=()=>{
-    navbar.classList.remove('active')
- }
+//get login button
+
+    let login = document.querySelector('.logbutton button');
+    let register = document.querySelector('.regbutton button');
+    login.style.color="black";
+    register.style.color="black";
+
+// onclick register button
+    register.onclick=()=>{
+        login.classList.remove('active');
+        register.classList.add('active');
+        document.querySelector('.account-form .login-form').classList.remove('active');
+        document.querySelector('.account-form .Register-form').classList.add('active');
+    }
+    //
+
+    // onclick login button
+    login.onclick=()=>{
+
+        register.classList.remove('active');
+        login.classList.add('active');
+        document.querySelector('.account-form .Register-form').classList.remove('active');
+        document.querySelector('.account-form .login-form').classList.add('active');
+       
+    }
+    // active account form
+    let accoutform= document.querySelector('.account-form');
+    document.querySelector('#account-btn').onclick=()=>{
+        accoutform.classList.add('active');
+    }
+
+    document.querySelector('#close-form').onclick=()=>{
+  
+        accoutform.classList.remove('active');
+    }
